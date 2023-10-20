50b31c7334932dea948c483746a824c68263ac38
SofiBec <sofibechardo@gmail.com>
2023-10-20 13:58:01 -0300

    Control de errores en el update

--- controller.js
+++ controller.js
@@ -33,7 +33,7 @@ class LibroController {
 
 
     /* -- ACTIVIDAD INTEGRADORA -- */
-    /* Actividad 1 Punto A */
+    /* Actividad 1 Punto A y D */
 
     async add(req, res) {
         const libro = req.body;
@@ -50,15 +50,26 @@ class LibroController {
 
     }
 
-    /* Actividad 1 Punto B */
+    /* Actividad 1 Punto B y D */
 
     async update(req, res) {
         const libro = req.body;
-        const [result] = await pool.query(`UPDATE libros SET nombre=(?), autor=(?), categoria=(?), ano_publicacion=(?), ISBN=(?) WHERE id_libro=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.ano_publicacion, libro.ISBN, libro.id_libro]);
-        res.json({ "Registros actualizados": result.changedRows });
-    }
 
-    /* Actividad 1 Punto C */
+        try {
+            const [result] = await pool.query(`UPDATE libros SET nombre=(?), autor=(?), categoria=(?), ano_publicacion=(?), ISBN=(?) WHERE id_libro=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.ano_publicacion, libro.ISBN, libro.id_libro]);
+            return res.json({ "Registros actualizados": result.changedRows });
+            
+        
+        } catch (err) {
+                console.log(err);
+                const Error = 'Libro inexistente, verificar los datos ingresados';
+                res.status(400).json({ Error });
+            
+            }
+        }
+ 
+
+    /* Actividad 1 Punto C y D */
 
     async delete(req, res) {
         const libro = req.body;
