69609a20d6a879118d15d1136d47d96e3bafc888
SofiBec <sofibechardo@gmail.com>
2023-10-24 11:02:55 -0300

    Entregable. Version 2.0

--- controller.js
+++ controller.js
@@ -36,16 +36,14 @@ class LibroController {
     /* Actividad 1 Punto A y D */
 
     async add(req, res) {
-        const libro = req.body;
-
         try {
+            const libro = req.body;
             const [result] = await pool.query(`INSERT INTO Libros(nombre, autor, categoria, ano_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.ano_publicacion, libro.ISBN]);
             res.json({ "id insertado": result.insertId });
 
         } catch (err) {
-            console.log(err);
-            const Error = 'Atributo inexistente, verificar el modelo de datos';
-            res.status(400).json({ Error });
+            console.error(err);
+            res.status(400).json({ Error: 'Libro inexistente, verificar los datos ingresados' });
         }
 
     }
@@ -53,36 +51,32 @@ class LibroController {
     /* Actividad 1 Punto B y D */
 
     async update(req, res) {
-        const libro = req.body;
-
         try {
+            const libro = req.body;
             const [result] = await pool.query(`UPDATE libros SET nombre=(?), autor=(?), categoria=(?), ano_publicacion=(?), ISBN=(?) WHERE id_libro=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.ano_publicacion, libro.ISBN, libro.id_libro]);
-            return res.json({ "Registros actualizados": result.changedRows });
-            
-        
-        } catch (err) {
-             console.log(err);
-             const Error = 'Libro inexistente, verificar los datos ingresados';
-             res.status(400).json({ Error });
-            
+            if (result.changedRows === 0) {
+                throw new Error('No se encontró el libro');
             }
+            res.json({ "Registros Actualizados": result.changedRows });
+
+        } catch (err) {
+            console.error(err);
+            res.status(400).json({ Error: 'Libro inexistente, verificar los datos ingresados' });
         }
+    }
  
 
     /* Actividad 1 Punto C y D */
 
     async delete(req, res) {
-        const libro = req.body;
-
         try {
+            const libro = req.body;
             const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN=(?)`, [libro.ISBN]);
             res.json({ "Registros eliminados": result.affectedRows });
 
         } catch (err) {
-            console.log(err);
-            const Error = 'Libro inexistente, verificar los datos ingresados';
-            res.status(400).json({ Error });
-
+            console.error(err);
+            res.status(400).json({ Error: 'Libro inexistente, verificar los datos ingresados' });
         }
     }
 
